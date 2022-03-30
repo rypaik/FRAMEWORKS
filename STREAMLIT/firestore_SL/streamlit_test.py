582b16bdfdcf861521c2237d5a3fbba90bdf2b0b
Ryan paik <ryanpaik@protonmail.com>
2022-03-30 10:00:18 -0700

    sync push

---
+++ STREAMLIT/firestore_SL/streamlit_test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Hello Universe!')
+if st.button('ballons?'):
+    st.button()
+
+
---
+++ STREAMLIT/streamlit_reddit/streamlit_test.py
@@ -0,0 +1,7 @@
+import streamlit as st
+
+st.header('Hello Universe!')
+if st.button('balloons?'):
+    st.button()
+
+
