ef8203fd5b08135cec3ec4d84d1318b2e1c94864
Ryan paik <ryanpaik@protonmail.com>
2022-04-22 16:19:05 -0700

    MBAir sync action 2

---
+++ STREAMLIT/streamlit_states/counter_states.py
@@ -0,0 +1,13 @@
+# Streamlit Counter 
+
+import streamlit as st
+
+st.title('Counter Example with States')
+if 'count' not in st.session_state:
+    st.session_state.count = 0
+
+increment = st.button('Increment')
+if increment:
+    st.session_state.count += 1
+
+st.write('Count = ', st.session_state.count)
