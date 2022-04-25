e9a0384954f146d9e948a4dac313ffc3254dc46a
rypaik <ryanpaik@protonmail.com>
2022-04-25 13:08:04 -0700

    linted streamlit reddit

--- STREAMLIT/streamlit_reddit/streamlit_app.py
+++ STREAMLIT/streamlit_reddit/streamlit_app.py
@@ -20,11 +20,7 @@ st.write("The contents are: ", doc.to_dict())
 doc_ref = db.collection("posts").document("Apple")
 
 # And then uploading some data to that reference
-doc_ref.set({
-	"title": "Apple",
-	"url": "www.apple.com"
-})
-
+doc_ref.set({"title": "Apple", "url": "www.apple.com"})
 
 
 # READ ALL POSTS
@@ -33,8 +29,8 @@ posts_ref = db.collection("posts")
 
 # For a reference to a collection, we use .stream() instead of .get()
 for doc in posts_ref.stream():
-	st.write("The id is: ", doc.id)
-	st.write("The contents are: ", doc.to_dict())
+    st.write("The id is: ", doc.id)
+    st.write("The contents are: ", doc.to_dict())
 
 
 # STREAMLIT SIDETS AND FIRESTORE
@@ -45,18 +41,15 @@ submit = st.button("Submit new post")
 
 # Once the user has submitted, upload it to the database
 if title and url and submit:
-	doc_ref = db.collection("posts").document(title)
-	doc_ref.set({
-		"title": title,
-		"url": url
-	})
+    doc_ref = db.collection("posts").document(title)
+    doc_ref.set({"title": title, "url": url})
 
 # And then render each post, using some light Markdown
 posts_ref = db.collection("posts")
 for doc in posts_ref.stream():
-	post = doc.to_dict()
-	title = post["title"]
-	url = post["url"]
+    post = doc.to_dict()
+    title = post["title"]
+    url = post["url"]
 
-	st.subheader(f"Post: {title}")
-	st.write(f":link: [{url}]({url})")
+    st.subheader(f"Post: {title}")
+    st.write(f":link: [{url}]({url})")
