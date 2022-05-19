8e235dd6468287ac5f594b8aac761ad3c602ffa4
SlushieCM <swingology19@gmail.com>
2022-05-19 15:55:23 -0700

    linux sqlachemy and flask

---
+++ FLASK/flask_sqlalc/.history/02_model_20220519123122.py
@@ -0,0 +1,36 @@
+from flask import Flask 
+from flask_sqlalchemy import SQLAlchemy
+
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///db.sqlite3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False 
+db = SQLAlchemy(app)
+#db.init_app(app)
+
+
+
+
+
+class Customer(db.Model):
+    id = db.Column(db.Integer, primary_key=True) 
+    first_name = db.Column(db.String(50), nullable=False)
+    last_name = db.Column(db.String(50), nullable=False)
+    address = db.Column(db.String(500), nullable=False)
+    city = db.Column(db.String(50), nullable=False)
+    postcode = db.Column(db.String(50), nullable=False)
+    email = db.Column(db.String(50), nullable=False, unique=True)
+    
+
+
+
+
+class Order(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    
+    
+    
+class Product(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(50), nullable=False, unique=True)
+    price = db.Column(db.Integer, nullable=False)
